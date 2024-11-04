3acdc0a9aff8136854865649128dae782d13b7e2
sh1raf <mr.kir05@mail.ru>
2024-11-04 17:28:25 +0700

    scene

--- Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
@@ -13,9 +13,9 @@ public class DungeonGenerator : MonoBehaviour
         Vector2 lastPos = transform.position;
         for(int i = 0; i < RoomsCount; i++)
         {
-            nextPos = rooms[i].Size / 2;
-
             Room room = Instantiate(rooms[Random.Range(0, rooms.Count)]);
+            nextPos = room.Size / 2;
+
             room.transform.position = nextPos;
             room.transform.parent = transform;
             lastPos = nextPos;
